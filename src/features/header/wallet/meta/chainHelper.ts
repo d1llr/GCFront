2afc9a6b0168e4f12b05f9f7eb592efa541a5950
GrandF17 <v.o.nikolenko@gmail.com>
2023-12-26 18:29:46 +0300

    MM add chain

--- src/features/header/wallet/meta/chainHelper.ts
+++ src/features/header/wallet/meta/chainHelper.ts
@@ -12,27 +12,49 @@ export async function changeChain(to: ChainId): Promise<boolean> {
         params: [{ chainId: hexId(Number(to)) }],
       })
       return true
-    } catch (error) {
-      if ((error as any).code === 4902) {
-        const result = async () => {
-          const param = {
-            chainId: hexId(Number(to)),
-            chainName: NETWORKS[to].name,
-            nativeCurrency: {
-              name: NETWORKS[to].symbol,
-              symbol: NETWORKS[to].symbol,
-              decimals: 18,
-            },
-            rpcUrls: [NETWORKS[to].rpc],
-            blockExplorerUrls: [NETWORKS[to].scanner],
-          }
-          await window.ethereum?.request?.({
+    } catch (e) {
+      console.error("Error while 'switch chain': ", e)
+      if ((e as any).code === 4902) {
+        // const result = async () => {
+        //   const param = {
+        //     chainId: hexId(Number(to)),
+        //     chainName: NETWORKS[to].name,
+        //     nativeCurrency: {
+        //       name: NETWORKS[to].symbol,
+        //       symbol: NETWORKS[to].symbol,
+        //       decimals: 18,
+        //     },
+        //     rpcUrls: [NETWORKS[to].rpc],
+        //     blockExplorerUrls: [NETWORKS[to].scanner],
+        //   }
+        //   await window.ethereum?.request?.({
+        //     method: "wallet_addEthereumChain",
+        //     params: [param],
+        //   })
+        // }
+        try {
+          await window.ethereum.request({
             method: "wallet_addEthereumChain",
-            params: [param],
+            params: [
+              {
+                chainId: hexId(Number(to)),
+                chainName: NETWORKS[to].name,
+                rpcUrls: [NETWORKS[to].rpc],
+                // iconUrls: [
+                //   "https://xdaichain.com/fake/example/url/xdai.svg",
+                //   "https://xdaichain.com/fake/example/url/xdai.png",
+                // ],
+                nativeCurrency: {
+                  name: NETWORKS[to].symbol,
+                  symbol: NETWORKS[to].symbol,
+                  decimals: 18,
+                },
+                blockExplorerUrls: [NETWORKS[to].scanner],
+              },
+            ],
           })
-        }
-
-        if (!result) {
+        } catch (e) {
+          console.error("Error while 'add chain': ", e)
           return false
         }
       }
