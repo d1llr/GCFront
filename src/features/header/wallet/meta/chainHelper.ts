94d735018e798c24d2d94745a790a4f327f3a22e
GrandF17 <v.o.nikolenko@gmail.com>
2023-12-26 18:44:16 +0300

    MM add chain

--- src/features/header/wallet/meta/chainHelper.ts
+++ src/features/header/wallet/meta/chainHelper.ts
@@ -11,27 +11,9 @@ export async function changeChain(to: ChainId): Promise<boolean> {
         method: "wallet_switchEthereumChain",
         params: [{ chainId: hexId(Number(to)) }],
       })
-      return true
     } catch (e) {
       console.error("Error while 'switch chain': ", e)
       if ((e as any).code === 4902) {
-        // const result = async () => {
-        //   const param = {
-        //     chainId: hexId(Number(to)),
-        //     chainName: NETWORKS[to].name,
-        //     nativeCurrency: {
-        //       name: NETWORKS[to].symbol,
-        //       symbol: NETWORKS[to].symbol,
-        //       decimals: 18,
-        //     },
-        //     rpcUrls: [NETWORKS[to].rpc],
-        //     blockExplorerUrls: [NETWORKS[to].scanner],
-        //   }
-        //   await window.ethereum?.request?.({
-        //     method: "wallet_addEthereumChain",
-        //     params: [param],
-        //   })
-        // }
         try {
           await window.ethereum.request({
             method: "wallet_addEthereumChain",
@@ -40,10 +22,6 @@ export async function changeChain(to: ChainId): Promise<boolean> {
                 chainId: hexId(Number(to)),
                 chainName: NETWORKS[to].name,
                 rpcUrls: [NETWORKS[to].rpc],
-                // iconUrls: [
-                //   "https://xdaichain.com/fake/example/url/xdai.svg",
-                //   "https://xdaichain.com/fake/example/url/xdai.png",
-                // ],
                 nativeCurrency: {
                   name: NETWORKS[to].symbol,
                   symbol: NETWORKS[to].symbol,
@@ -57,12 +35,14 @@ export async function changeChain(to: ChainId): Promise<boolean> {
           console.error("Error while 'add chain': ", e)
           return false
         }
+      } else {
+        return false
       }
-      return true
     }
   } catch (e) {
     console.error(e)
+    return false
   }
 
-  return false
+  return true
 }
