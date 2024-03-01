89d75b35b4034b6a1803c870d9e95fdf429f918e
Evgenixs <les_zhenya@mail.ru>
2024-03-01 11:32:57 +0300

    RegConfPage

--- src/features/user/login/Attention.tsx
+++ src/features/user/login/Attention.tsx
@@ -1,17 +1,19 @@
 const Attention = () => {
     return (
-        <div className="rounded-3xl w-[520px] h-[313px]  flex flex-col gap-2 p-4 items-center bg-slate-800">
-            <div className="text-white font-orbitron font-bold pt-4 text-2xl">Attention!</div>
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
+            <div className="rounded-3xl w-[520px] h-[313px] flex flex-col gap-2 p-4 items-center bg-[#272727]">
+        <div className="text-white font-orbitron font-bold pt-4 text-2xl">Attention!</div>
 
-            <main className=" text-white text-center ">
-            <ol className="text-[14px] m-3 ">
-                <li className="">Take into account all commissions when making a transfer. If the amount is less or more than 10 PAC, the payment will not be credited automatically</li>
-                <li className="mt-10">Send only USDT TRC20. Sending any other coin may result in its loss</li>
-           </ol>
-            
-            </main>
-            <button className=" rounded-xl font-bold text-lg font-orbitron bg-yellow w-[322px] h-[48px]">Accept</button>
-        </div>
+        <main className=" text-white text-center ">
+        <ol className="text-[14px] m-3 ">
+            <li className="">Take into account all commissions when making a transfer. If the amount is less or more than 10 PAC, the payment will not be credited automatically</li>
+            <li className="mt-10">Send only USDT TRC20. Sending any other coin may result in its loss</li>
+       </ol>
+        
+        </main>
+        <button className=" rounded-xl font-bold text-lg font-orbitron bg-yellow w-[322px] h-[48px]">Accept</button>
+    </div></div>
+        
       
     )
 }
--- src/features/user/login/RegConf.tsx
+++ src/features/user/login/RegConf.tsx
@@ -1,6 +1,21 @@
-const RegConf=()=>{
-    return(
-        <div className="text-white">gfdgfdgfdg</div>
+import SucessRegister from '../../../images/icons/SucessRegister.svg'
+const RegConf = () => {
+    return (
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
+            <div className="rounded-3xl w-[386px] h-[278px] flex flex-col gap-2 p-6 items-center bg-[#272727]">
+                <div className="text-center text-white text-[24px] font-orbitron  font-bold">
+                    You are registred
+                    <div className='p-6 flex flex-col justify-center items-center'>
+                        <img src={SucessRegister}alt="Sucess register"/>
+                    </div>
+                    </div>
+                    <div>
+                        <button className='font-orbitron text-[20px] font-bold text-black rounded-xl bg-yellow w-[322px] h-[50px]'>log in</button>
+                    </div>
+                </div>
+            
+
+        </div>
     )
 }
 export default RegConf
