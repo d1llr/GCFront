eb9c4ce29e30b888e1d36f4ab24edb1408dfdea4
Evgenixs <les_zhenya@mail.ru>
2024-03-01 12:01:17 +0300

    RegConfChanged

--- src/features/user/login/RegConf.tsx
+++ src/features/user/login/RegConf.tsx
@@ -2,18 +2,18 @@ import SucessRegister from '../../../images/icons/SucessRegister.svg'
 const RegConf = () => {
     return (
         <div className="w-full flex flex-col gap-4 justify-center items-center">
-            <div className="rounded-3xl w-[386px] h-[278px] flex flex-col gap-2 p-6 items-center bg-[#272727]">
-                <div className="text-center text-white text-[24px] font-orbitron  font-bold">
+            <div className="rounded-3xl max-w-[386px] w-full flex flex-col gap-7 p-6 items-center bg-[#272727]">
+                
+                <h1 className="text-center text-white text-2xl font-orbitron font-bold">
                     You are registred
-                    <div className='p-6 flex flex-col justify-center items-center'>
-                        <img src={SucessRegister}alt="Sucess register"/>
-                    </div>
-                    </div>
-                    <div>
-                        <button className='font-orbitron text-[20px] font-bold text-black rounded-xl bg-yellow w-[322px] h-[50px]'>log in</button>
-                    </div>
-                </div>
-            
+                </h1>
+
+                <img src={SucessRegister} alt="Sucess register" />
+
+                <button className='p-2 font-orbitron text-xl font-bold text-black rounded-xl bg-yellow w-full'>log in</button>
+
+            </div>
+
 
         </div>
     )
