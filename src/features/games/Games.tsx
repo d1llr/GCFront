2a32408cee2deea0414e3ec83642508f34b1cb8d
Ilya <85482397+Fenoty@users.noreply.github.com>
2024-03-03 20:02:53 +0300

    games page complete, tournament page not complete

--- src/features/games/Games.tsx
+++ src/features/games/Games.tsx
@@ -18,47 +18,37 @@ const Games = () => {
     <div className="wrapper-content">
       <h1 className="font-orbitron w-fit text-yellow text-8xl font-extrabold">Games</h1>
 
-      <div className="grid grid-cols-3 2xl:gap-7 mt-10 md:gap-3">
+      <div className="grid grid-cols-3 gap-7 mt-10">
         {data?.map((item: IGames, index: number) => {
           return (
             <div key={index} className="bg-yellow bg-gameShards bg-no-repeat bg-right-top rounded-[20px] h-full">
 
-              <div className="
-                  game-name
-
-                  text-white 
-                  font-orbitron 
-                  py-3 pl-6 
-                  text-2xl 
-                  max-w-[50%] 
-                  bg-customBlack 
-                  rounded-br-3xl
-                ">
-                {item.name}
-              </div>
-
-              <div className="h-full overflow-hidden flex flex-col justify-between gap-4 py-6 px-6">
-                <div className="text-customBlack text-xl">
-                  {item.short_desc}
+              <div className="h-full overflow-hidden flex flex-col gap-4">
+                <div className="game-name text-white font-orbitron py-3 px-6 text-3xl max-w-fit bg-customBlack rounded-br-3xl">
+                  {item.name}
                 </div>
 
-                <button
-                  onClick={() => {
-                    navigate(`/games/${item.id}`)
-                  }}
-                  disabled={!item.active}
+                <div className="h-full overflow-hidden flex flex-col justify-between gap-6">
 
-                  className="w-full bg-customBlack text-yellow rounded-3xl mt-8 2xl:text-xl md:text-base font-bold 2xl:p-3 md:p-2 text-center cursor-pointer disabled:opacity-30"
-                >
-                  More detailed
-                </button>
+                  <div className="text-customBlack text-xl py-6 px-6 pb-0 font-medium">
+                    {item.short_desc}
+                  </div>
+
+                  <div className="py-6 px-6 pt-0">
+                    <button
+                      onClick={() => {
+                        navigate(`/games/${item.id}`)
+                      }}
+                      disabled={!item.active}
+                      className="w-full bg-customBlack text-yellow rounded-3xl 2xl:text-xl md:text-base font-bold 2xl:p-3 md:p-2 text-center cursor-pointer disabled:opacity-30"
+                    >
+                      More detailed
+                    </button>
+                  </div>
+                  
+                </div>
+
               </div>
-
-
-
-
-
-
 
             </div>
           )
@@ -69,17 +59,3 @@ const Games = () => {
 }
 
 export default Games
-
-
-{/* <div className="flex flex-col gap-2">
-                  <img
-                    src={"https://back.pacgc.pw" + item.image}
-                    alt="Фото игры"
-                  />
-                  <span className="text-yellow 2xl:text-2xl md:text-xl">
-                    {item.name}
-                  </span>
-                  <span className="text-white 2xl:text-xl md:text-base">
-                    {item.short_desc}
-                  </span>
-                </div> */}
--- src/features/tournaments/Tournaments.tsx
+++ src/features/tournaments/Tournaments.tsx
@@ -5,35 +5,39 @@ import { useNavigate } from "react-router-dom";
 import Loader from "../../helpers/Loader";
 import Error from "../../helpers/Error";
 const Tournaments = () => {
-    const { data, isLoading, isError, error } = useGetActiveTournamentsQuery()
-    const { data: HistoryData } = useGetHistoryTournamentsQuery()
+    // const { data, isLoading, isError, error } = useGetActiveTournamentsQuery()
+    // const { data: HistoryData } = useGetHistoryTournamentsQuery()
+
+    const data = [{ "image": "/storage/tournaments/octa.svg", "disabled": false, "name": "OCTA Tournament", "description": "3 in row", "map": "3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,137,139,141,143,145,147,149,151,153,155,157,159,161,163,165,167,169,171,173,175,177,179,181,183,185,187,189,191,193,195,197,199,204", "id": "1", "players": "1,9,12,85,75,20,117,29,104", "address": "0x63b3B5a9113D5e3e9cF50c2Ab619d89e8d8D7DA9", "chainID": "800001", "cost": 5, "game": "3inRow", "daysLeft": "7", "dayOfWeekFrom": "wednesday", "dayOfWeekTo": "Thursday", "tournament_key": "90a6ef738380cc48f90e", "goal": "Description", "participants": "9", "bank": "80", "createdAt": "2024-02-29T13:02:00.000Z", "updatedAt": "2024-02-29T13:02:00.000Z" }, { "image": "/storage/tournaments/octa.svg", "disabled": false, "name": "OCTA Tournament", "description": "3 in row", "map": "3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,137,139,141,143,145,147,149,151,153,155,157,159,161,163,165,167,169,171,173,175,177,179,181,183,185,187,189,191,193,195,197,199,204", "id": "2", "players": "12,29,85,153,146,117,20,109", "address": "0x63b3B5a9113D5e3e9cF50c2Ab619d89e8d8D7DA9", "chainID": "800001", "cost": 5, "game": "3inRow", "daysLeft": "7", "dayOfWeekFrom": "friday", "dayOfWeekTo": "sunday", "tournament_key": "43e50bc1b4bf2037095a", "goal": "Description", "participants": "8", "bank": "80", "createdAt": "2024-03-03T15:00:00.000Z", "updatedAt": "2024-03-03T15:00:00.000Z" }];
+    const HistoryData = [{ "image": "/storage/tournaments/octa.svg", "disabled": false, "name": "OCTA Tournament", "description": "3 in row", "map": "3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,137,139,141,143,145,147,149,151,153,155,157,159,161,163,165,167,169,171,173,175,177,179,181,183,185,187,189,191,193,195,197,199,204", "id": "1", "players": "1,9,12,85,75,20,117,29,104", "address": "0x63b3B5a9113D5e3e9cF50c2Ab619d89e8d8D7DA9", "chainID": "800001", "cost": 5, "game": "3inRow", "daysLeft": "7", "dayOfWeekFrom": "wednesday", "dayOfWeekTo": "Thursday", "tournament_key": "90a6ef738380cc48f90e", "goal": "Description", "participants": "9", "bank": "80", "createdAt": "2024-02-29T13:02:00.000Z", "updatedAt": "2024-02-29T13:02:00.000Z" }, { "image": "/storage/tournaments/octa.svg", "disabled": false, "name": "OCTA Tournament", "description": "3 in row", "map": "3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,137,139,141,143,145,147,149,151,153,155,157,159,161,163,165,167,169,171,173,175,177,179,181,183,185,187,189,191,193,195,197,199,204", "id": "2", "players": "12,29,85,153,146,117,20,109", "address": "0x63b3B5a9113D5e3e9cF50c2Ab619d89e8d8D7DA9", "chainID": "800001", "cost": 5, "game": "3inRow", "daysLeft": "7", "dayOfWeekFrom": "friday", "dayOfWeekTo": "sunday", "tournament_key": "43e50bc1b4bf2037095a", "goal": "Description", "participants": "8", "bank": "80", "createdAt": "2024-03-03T15:00:00.000Z", "updatedAt": "2024-03-03T15:00:00.000Z" }];
 
     const navigate = useNavigate();
 
-    if (isLoading) {
-        return <Loader />
-    }
+
+    // if (isLoading) {
+    //     return <Loader />
+    // }
     return (
-        <div>
-            <h2 className="w-fit decoration-dotted underline text-yellow text-2xl">
-                Tournaments
-            </h2>
-            <div className="grid grid-cols-2 gap-6 mt-10">
+        <div className="wrapper-content">
+            <h1 className="font-orbitron w-fit text-yellow text-8xl font-extrabold">Games</h1>
+
+            <div className="grid grid-cols-3 gap-6 mt-10">
                 {data?.map((item: ITournaments, index: number) => {
                     return (
-                        <div key={index} className="border-yellow border-2 p-3 flex flex-row gap-2 text-white">
-                            <div className="w-2/3">
-                                <img src={ import.meta.env.VITE_BACKEND_URL + item.image} alt="Фото игры" width={345} height={345} className="object-cover w-full h-full max-h-80" />
-                            </div>
+                        <div key={index} className="bg-lightGray p-6 rounded-[20px] flex flex-row gap-2 text-white">
+
+
+
                             <div className="flex flex-col w-full gap-6 justify-center">
                                 <div>
                                     <div className="flex flex-row justify-between items-center gap-2" >
-                                        <span className="text-yellow text-3xl">
-                                            {item.name} | {item.id}
-                                        </span>
-                                        <span className="text-gray">
-                                            {item.daysLeft} days
-                                        </span>
+                                        <div className="text-white font-orbitron text-3xl flex flex-col items-start">
+                                            <div>{item.name}</div>
+                                            <div>{item.id}</div>
+                                        </div>
+                                        <div className="text-gray">
+                                            28.03
+                                        </div>
                                     </div>
                                     <span className="text-white text-base break-normal">
                                         {item.description}
@@ -82,7 +86,7 @@ const Tournaments = () => {
                     return (
                         <div key={index} className="border-yellow border-2 p-3 flex flex-row gap-2 text-white opacity-30">
                             <div className="w-2/3">
-                                <img src={ import.meta.env.VITE_BACKEND_URL + item.image} alt="Фото игры" width={345} height={345} className="object-cover w-full h-full max-h-80" />
+                                <img src={import.meta.env.VITE_BACKEND_URL + item.image} alt="Фото игры" width={345} height={345} className="object-cover w-full h-full max-h-80" />
                             </div>
                             <div className="flex flex-col w-full gap-6 justify-center">
                                 <div>
@@ -139,8 +143,23 @@ const Tournaments = () => {
                 })}
                 {(data?.length == 0 && HistoryData?.length == 0) && <Error />}
             </div>
+
         </div>
+
     );
 }
 
 export default Tournaments;
+
+
+
+{/* <div>
+            <h2 className="w-fit decoration-dotted underline text-yellow text-2xl">
+                Tournaments
+            </h2>
+            
+        </div> */}
+
+{/* <div className="w-2/3">
+    <img src={import.meta.env.VITE_BACKEND_URL + item.image} alt="Фото игры" width={345} height={345} className="object-cover w-full h-full max-h-80" />
+</div> */}
